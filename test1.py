361fe737f62e8db0183d74bb9ddfc4fcf2561038
IseulCeylon19 <IseulCeylon19@ioiio.cc>
2025-05-31 19:24:38 +0700

    test3

--- test1.py
+++ test1.py
@@ -1,3 +1,13 @@
 import tensorflow as tf  # now import the tensorflow module
 # print(tf.version)  # make sure the version is 2.x
 print(tf.__version__)  # print the version of TensorFlow
+print(tf.version.VERSION)
+
+t = tf.zeros([2, 3])  # create a tensor of zeros with shape [2, 3]
+
+print(t.shape, t.dtype)
+print(t.numpy())
+print(t.numpy().shape, t.numpy().dtype, t.numpy().tolist(), t.numpy().tolist()[0])
+
+t = tf.reshape(t, [1, -1])
+print(t)
--- test2.py
+++ test2.py
@@ -1,17 +1,11 @@
 import tensorflow as tf  # now import the tensorflow module
-print(tf.version.VERSION)  # make sure the version is 2.x / print(tf.__version__) 
+import matplotlib.pyplot as plt
+import numpy as np
 
-t = tf.zeros([2, 3])  # create a tensor of zeros with shape [2, 3]
+x = [1, 2, 2.5, 3, 4]
+y = [1, 4, 7, 9, 15]
 
-print(t.shape)  # print the shape of the tensor
-print(t.dtype)  # print the data type of the tensor
-print(t.numpy())  # convert the tensor to a numpy array and print it
-print(t.numpy().shape)  # print the shape of the numpy array
-print(t.numpy().dtype)  # print the data type of the numpy array
-print(t.numpy().tolist())  # convert the numpy array to a list and print it
-print(t.numpy().tolist()[0])  # print the first element of the list
-print(t.numpy().tolist()[0][0])  # print the first element of the first row of the list
-print(t.numpy().tolist()[0][0] == 0)  # check if the first element of the first row is 0
-
-t = tf.reshape(t, [1, -1])
-print(t)
+plt.plot(x, y, 'ro')
+plt.axis([0, 6, 0, 20])
+plt.plot(np.unique(x), np.poly1d(np.polyfit(x, y, 1))(np.unique(x)))
+plt.show()
