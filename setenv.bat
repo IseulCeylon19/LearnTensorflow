9c23a0eae771094fd56c18d1c6de0d32c169dd28
IseulCeylon19 <IseulCeylon19@ioiio.cc>
2025-06-04 16:05:21 +0700

    add m activate

---
+++ setenv.bat
@@ -0,0 +1,16 @@
+set PATH=C:\Windows\System32;C:\Windows\System32\wbem;C:\IseulCeylon19\dev\IseulCeylon19\AppData\Roaming;C:\IseulCeylon19\Git-2.49.0-64-bit\cmd;C:\IseulCeylon19\Git-2.49.0-64-bit\usr\bin;C:\IseulCeylon19\micromamba-win-64;C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v11.8\bin;C:\cudnn-windows-x86_64-8.9.7.29_cuda11\bin;
+set ALLUSERSPROFILE=C:\IseulCeylon19\dev\IseulCeylon19\ProgramData
+set APPDATA=C:\IseulCeylon19\dev\IseulCeylon19\AppData\Roaming
+set COMPUTERNAME=IseulCeylon19
+set HOMEDRIVE=C:
+set HOMEPATH=C:\IseulCeylon19\dev\IseulCeylon19
+set HOME=C:\IseulCeylon19\dev\IseulCeylon19
+set USER_HOME=C:\IseulCeylon19\dev\IseulCeylon19
+set LOCALAPPDATA=C:\=IseulCeylon19\dev\IseulCeylon19\AppData\Local
+set LOGONSERVER=\\IseulCeylon19
+set USERNAME=IseulCeylon19
+set USERPROFILE=C:\IseulCeylon19\dev\IseulCeylon19
+set TMP=C:\IseulCeylon19\dev\IseulCeylon19\AppData\Local\Temp
+set TEMP=C:\IseulCeylon19\dev\IseulCeylon19\AppData\Local\Temp
+set MYSQL_HOME=C:\IseulCeylon19\mysql-8.4.3-winx64
+set MAMBA_ROOT_PREFIX=C:\IseulCeylon19\micromamba-win-64
