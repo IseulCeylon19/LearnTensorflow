55f05bd4d5202f7624ff89260a93769278db46d3
IseulCeylon19 <IseulCeylon19@ioiio.cc>
2025-06-13 12:30:37 +0700

    DS: update setenv

--- setenv.bat
+++ setenv.bat
@@ -1,4 +1,4 @@
-set PATH=C:\Windows\System32;C:\Windows\System32\wbem;C:\IseulCeylon19\dev\IseulCeylon19\AppData\Roaming;C:\IseulCeylon19\Git-2.49.0-64-bit\cmd;C:\IseulCeylon19\Git-2.49.0-64-bit\usr\bin;C:\IseulCeylon19\micromamba-win-64;C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v11.8\bin;C:\cudnn-windows-x86_64-8.9.7.29_cuda11\bin;
+set PATH=C:\IseulCeylon19\micromamba-win-64\condabin;C:\Windows\System32;C:\Windows\System32\wbem;C:\IseulCeylon19\dev\IseulCeylon19\AppData\Roaming;C:\IseulCeylon19\Git-2.49.0-64-bit\cmd;C:\IseulCeylon19\Git-2.49.0-64-bit\usr\bin;C:\IseulCeylon19\micromamba-win-64;C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v11.8\bin;C:\cudnn-windows-x86_64-8.9.7.29_cuda11\bin;
 set ALLUSERSPROFILE=C:\IseulCeylon19\dev\IseulCeylon19\ProgramData
 set APPDATA=C:\IseulCeylon19\dev\IseulCeylon19\AppData\Roaming
 set COMPUTERNAME=IseulCeylon19
@@ -14,3 +14,4 @@ set TMP=C:\IseulCeylon19\dev\IseulCeylon19\AppData\Local\Temp
 set TEMP=C:\IseulCeylon19\dev\IseulCeylon19\AppData\Local\Temp
 set MYSQL_HOME=C:\IseulCeylon19\mysql-8.4.3-winx64
 set MAMBA_ROOT_PREFIX=C:\IseulCeylon19\micromamba-win-64
+mamba_hook.bat
--- setenvS.bat
+++ setenvS.bat
@@ -1,4 +1,4 @@
-set PATH=C:\Windows\System32;C:\Windows\System32\wbem;S:\IseulCeylon19\dev\IseulCeylon19\AppData\Roaming;S:\IseulCeylon19\Git-2.49.0-64-bit\cmd;S:\IseulCeylon19\Git-2.49.0-64-bit\usr\bin;S:\IseulCeylon19\micromamba-win-64;C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v11.8\bin;C:\cudnn-windows-x86_64-8.9.7.29_cuda11\bin;
+set PATH=S:\IseulCeylon19\micromamba-win-64\condabin;C:\Windows\System32;C:\Windows\System32\wbem;S:\IseulCeylon19\dev\IseulCeylon19\AppData\Roaming;S:\IseulCeylon19\Git-2.49.0-64-bit\cmd;S:\IseulCeylon19\Git-2.49.0-64-bit\usr\bin;S:\IseulCeylon19\micromamba-win-64;C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v11.8\bin;C:\cudnn-windows-x86_64-8.9.7.29_cuda11\bin;
 set ALLUSERSPROFILE=S:\IseulCeylon19\dev\IseulCeylon19\ProgramData
 set APPDATA=S:\IseulCeylon19\dev\IseulCeylon19\AppData\Roaming
 set COMPUTERNAME=IseulCeylon19
@@ -14,3 +14,4 @@ set TMP=S:\IseulCeylon19\dev\IseulCeylon19\AppData\Local\Temp
 set TEMP=S:\IseulCeylon19\dev\IseulCeylon19\AppData\Local\Temp
 set MYSQL_HOME=S:\IseulCeylon19\mysql-8.4.3-winx64
 set MAMBA_ROOT_PREFIX=S:\IseulCeylon19\micromamba-win-64
+mamba_hook.bat
